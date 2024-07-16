b36a21342d642b81b808b06d7dd563c7f3b53281
ramakrishnarao679 <ramakrishnarao679@gmail.com>
2024-07-16 15:21:20 +0530

    palindrome is added

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+// function isPalindrome(str){
+//     let reversed=str.split('').reverse().join('');
+//     return str = reversed;
+// }
+// console.log(isPalindrome('racecar'));
+
+//////////////////////////////////////////////////////
+
+function isPalindrome(str){
+    let reversed=str.split('').reverse().join('');
+    return str === reversed;
+}
+console.log(isPalindrome('racecar'));
